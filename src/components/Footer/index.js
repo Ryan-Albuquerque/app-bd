6ea93f425b9ca2d213580ce7ef37919ca6814621
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2021-06-28 21:45:17 -0400

    footer and adjusting browse text

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Container } from 'react-bootstrap';
 
-function Footer() {
+
+function Footer (){
     return (
-        <div>
-            
-        </div>
+        <Container fluid className="d-flex py-2 justify-content-center bg-dark fs-10">
+            <h5 className="text-white">BD: Credit Products @ Ryan</h5>
+        </Container>
     )
 }
 
-export default Footer
+export default Footer;
