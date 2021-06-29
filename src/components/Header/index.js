7a71456fa8235f46dbd6efd54c11ddc3124578a5
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2021-06-28 21:41:12 -0400

    Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import {Container, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-function Header() {
+function Header(){
     return (
-        <div>
-            
-        </div>
+        <Container fluid className="d-flex justify-content-center bg-dark py-3">
+            <Link to="/">
+                <Image src="https://bemoldigital.com.br/images/logos/bemol-digital-inline-colorful.svg" width={175}/>
+            </Link>
+        </Container>
     )
 }
 
-export default Header
+export default Header;
